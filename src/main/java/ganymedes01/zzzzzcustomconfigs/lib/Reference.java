e1da29aa8b8871b26439e92dafbd772710484b3a
ganymedes01 <foka_12@hotmail.com>
2014-07-01 14:00:08 +0100

    Prep for release

--- src/main/java/ganymedes01/zzzzzcustomconfigs/lib/Reference.java
+++ src/main/java/ganymedes01/zzzzzcustomconfigs/lib/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 
 	public static final String MOD_ID = "zzzzzcustomconfigs";
 	public static final String MOD_NAME = "ZZZZZ Custom Configs";
-	public static final String VERSION_NUMBER = "1.3.1";
+	public static final String VERSION_NUMBER = "1.3.2";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.12.2.1124,);after:*";
 }
