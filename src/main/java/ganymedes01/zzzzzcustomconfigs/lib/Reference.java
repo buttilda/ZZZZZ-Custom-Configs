af92ddcc5f78245b75f7a64d74099db317af9e35
ganymedes01 <foka_12@hotmail.com>
2014-12-02 16:02:13 -0200

    Bump version

--- src/main/java/ganymedes01/zzzzzcustomconfigs/lib/Reference.java
+++ src/main/java/ganymedes01/zzzzzcustomconfigs/lib/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 
 	public static final String MOD_ID = "zzzzzcustomconfigs";
 	public static final String MOD_NAME = "ZZZZZ Custom Configs";
-	public static final String VERSION_NUMBER = "2.0.0";
+	public static final String VERSION_NUMBER = "2.0.1";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.13.2.1230,);after:*";
 }
