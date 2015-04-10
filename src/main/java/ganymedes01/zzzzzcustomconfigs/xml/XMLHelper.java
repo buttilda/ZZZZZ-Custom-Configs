a35483e939764b862473b7df575c4d356332a89b
ganymedes01 <foka_12@hotmail.com>
2015-04-10 11:01:20 -0300

    Depend on the latest forge with the fluid registry changes

--- src/main/java/ganymedes01/zzzzzcustomconfigs/xml/XMLHelper.java
+++ src/main/java/ganymedes01/zzzzzcustomconfigs/xml/XMLHelper.java
@@ -129,7 +129,7 @@ public class XMLHelper {
 			return "\"" + obj + "\"";
 		else if (obj instanceof FluidStack) {
 			FluidStack stack = (FluidStack) obj;
-			return FluidRegistry.getFluidName(stack.fluidID) + " " + stack.amount + (stack.tag != null ? " " + stack.tag : "");
+			return FluidRegistry.getFluidName(stack) + " " + stack.amount + (stack.tag != null ? " " + stack.tag : "");
 		} else if (obj instanceof Item)
 			return Item.itemRegistry.getNameForObject(obj);
 		else if (obj instanceof Block)
